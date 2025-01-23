3d61cabec5430c6b09c6cc8582e8b29d4c30cfe4
JinoMarquezz <jickzho@gmail.com>
2025-01-22 20:07:10 -0500

    Realizar la vista del menu

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,7 @@ const router=Router();
 
 router.get('/', (req,res) => res.render('index',{title:'Inicio'}))
 
+router.get('/menu', (req,res) => res.render('menu',{title:'Menu'}))
+
 
 export default router;
